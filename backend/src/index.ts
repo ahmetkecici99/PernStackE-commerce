5700bbba98cc844d0c11b64487324ec6a613bd0e
Ahmet Keçici <ahmetkecici99@gmail.com>
2026-07-30 20:57:56 +0300

    Webhook implemented

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,18 @@
-    console.log("Selamlar")
+import express from 'express';
+import cors from 'cors';
+import { clerkMiddleware } from '@clerk/express';
+import { clerkWebhookHandler } from './webhooks/clerk';
+import { getEnv } from './lib/env'
+const env = getEnv()
+const app = express();
+const rawJson = express.raw({ type: "application/json", limit: "1mb" })
+app.post("/webhooks/clerk", rawJson, (req, res) => {
+    void clerkWebhookHandler(req, res)
+})
+app.use(express.json())
+app.use(cors())
+app.use(clerkMiddleware());
+
+
+
+app.listen(env.PORT, () => console.log("listening in 30000"));
